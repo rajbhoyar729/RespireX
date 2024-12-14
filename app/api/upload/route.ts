7bb057728338cfb433cca4fb51bd4f1d7cca90d0
rajbhoyar729 <rabhoyarr729@gmail.com>
2024-12-14 19:27:33 +0530

    update

--- app/api/upload/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from "next/server";
-
-export async function POST(request:NextRequest) {
-    const data = await request.formData()
-    const file : File | null = data.get('file') as unknown as File
-    
-}
